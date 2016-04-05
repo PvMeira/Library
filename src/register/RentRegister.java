20a41c135eec3e8c818f968fa1624c8cf9a9d241
Pedro Vitor Meira <pv.ciria@gmail.com>
2016-04-05 19:35:39 -0300

    create amethod to print a note in rent()

--- src/register/RentRegister.java
+++ src/register/RentRegister.java
@@ -13,21 +13,24 @@ import util.TokenCreation;
 public class RentRegister {
 
 	TokenCreation t = new TokenCreation();
+	Client client;
+	Book book;
+	int cod1;
 
 	public void addNewRent(Client c, Book b) {
 		try {
 
 			Integer cod = ConsoleReader.scanInt("Type the ISBN of the book :");
-			Book book = BookRepository.getInstance().searchByCode(cod);
+			book = BookRepository.getInstance().searchByCode(cod);
 			if (b.isAvaliable() == true) {
 
 				String name = ConsoleReader.scanString("Type the Full name of the client ");
-				Client client = ClientRepository.getInstance().searchByName(name);
+				client = ClientRepository.getInstance().searchByName(name);
 				if (client.getToken() != null) {
 
 					if (c.getCountClientBooksRent() <= 3) {
 
-						int cod1 = t.codRentCreation();
+						cod1 = t.codRentCreation();
 						DeadLineCreation dead = new DeadLineCreation();
 						Rent r = new Rent(c, b, cod1, dead.getDateFormatter());
 						RentRepository.getInstance().add(r);
@@ -39,6 +42,7 @@ public class RentRegister {
 						System.out.println("Client " + client.getClientName() + "\nRent  was successful\n"
 								+ "Book Name :" + book.getBookName() + "The Date for the Client to return is :"
 								+ dead.createNewData());
+						pintNote();
 
 					} else {
 						System.out.println("Sorry, but this Client already has 3 books rent");
@@ -54,6 +58,17 @@ public class RentRegister {
 			System.out.println("TheISBN code wasn´t not found " + e);
 		}
 
+	}
+
+	public void pintNote() {
+		char op1 = ConsoleReader.scanChar("Print the note ?(y/n)");
+		if (op1 == 'y') {
+			System.out.println("\nClient Name: " + client.getClientName() + "\nBook Name: " + book.getBookName()
+					+ "\nRent Code: " + cod1);
+
+		} else {
+			System.out.println("___");
+		}
 	}
 
 }
